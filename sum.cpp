b1c116e2fb1c2b569c3cf54ed66507d16152c6e1
Tobias  Strobl <47507728-TobiasStrobl1@users.noreply.replit.com>
2026-01-21 07:27:53 +0000

    Inintial Commit

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int b;
+    int sum = 0;
+
+    while(scanf("%d",&b) != \0)
+        {
+            sum += b;
+        }
+
+    printf("%d",sum);
+        
+    return 0;
+} 
+
+
