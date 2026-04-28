7abb7776574c8be6d878ae646280e042fe3e421e
TobiasS3 <Tobias.Strobl1@htl-braunau.at>
2026-04-28 16:07:42 +0000

    Erweiterung

---
+++ berichte/summerie.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  int a = 0;
+  int b = 0;
+  int summe = 0;
+
+for(int i = a; i <= b; i++)
+{
+    summe += i;
+}
+  cout << summe;
+
+  return 0;
+}
